034d2813a3c6845cacd4614edeb4c52e9113d949
analauradias <analauramanfron@gmail.com>
2023-11-28 16:34:25 -0300

    consegui colocar como uma lista

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let texto = document.getElementById("#text")
-let adicionar = document.getElementById("#adicionar");
-let tarefas = document.getElementById("#tarefas"); 
+let texto = document.querySelector("#descricao");
+let adicionar = document.querySelector("#adicionar");
+let tarefas = document.querySelector("#tarefas");
+let finalizadas = document.querySelector("#finalizadas");
 
-function adicionadas(add) {
-    let tarefas = texto.value;
+adicionar.addEventListener("click", function () {
+    add(descricao.value);
+});
+
+
+function add(texto) {
+    if(texto != ''){
+        tarefas.innerHTML += '<li>' + texto + '-' + '<a href="#" id="finalizar" onclick(tirar)>FEITA</a>' + '</li>';
+    }
+}
+
+function tirar() {
+    alert(`fds`);
 }
